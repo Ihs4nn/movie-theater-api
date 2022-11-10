1aab412deb0d5b555f9ab5fa3a0ed3f25d732ea3
Ihs4nn <dfihsan@gmail.com>
2022-11-10 15:08:01 +0000

    Finished all the get code, going to test them in postman now

---
+++ routes/showR.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const showRouter = Router();
+const User = require("../models/User");
+const Show = require("../models/Show");
--- routes/userR.js
+++ routes/userR.js
@@ -1,17 +1,27 @@
-//defines routes
+//Defining section of the User Route
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
-//make function
+const Show = require("../models/Show");
+
+//Validation function for the PUT method
 
 //GET all User
 userRouter.get("/", async (req, res) => {
     const allUser = await User.findall();
     res.send(allUser);
 })
-
 //GET one User
+userRouter.get("/:num", async (req, res) => {
+    const oneUser = await User.findByPk(req.params.num);
+    res.send(oneUser);
+})
 
 //GET all shows watched by a User [id.params.req]
-
+userRouter.get("/:num/shows", async (req, res) => {
+    const findShows = await Show.findAll({
+        where: {userId: req.params.num}
+    })
+    res.send(findShows);
+})
 //PUT update and add a show if a User has watched it
