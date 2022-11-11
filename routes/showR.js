5235c7888ae8a9780d8302bb8cd1b12f983481f0
Ihs4nn <dfihsan@gmail.com>
2022-11-11 10:02:15 +0000

    Beginning to add code to the Show routes

--- routes/showR.js
+++ routes/showR.js
@@ -1,4 +1,22 @@
+//Home of the definitions
 const { Router } = require("express");
 const showRouter = Router();
-const { User } = require("../models/User");
-const { Show } = require("../models/Show");
+const { User, Show } = require("../models");
+
+//Validation functions
+
+//GET all Shows
+
+//GET one Shows
+
+//GET Shows in a particular genre [req.params]
+
+//PUT updating rating of a Show that has been watched
+
+//PUT update the status of a Show
+
+//DELETE a Show
+
+
+
+module.exports = showRouter;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userR");
-// const showsRouter = require("./routes/showR")
+const showRouter = require("./routes/showR")
 const db = require("./db/db");
 
 app.use(express.json());
 
 app.use("/users", userRouter);
-// app.use("/shows", showsRouter);
+app.use("/shows", showRouter);
 
 app.listen(3000, async () => {
     console.log("Listening on port 3000");
