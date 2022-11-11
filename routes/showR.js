6200f3a54f85d483e751d56fc5b7c33b0ada79b7
Ihs4nn <dfihsan@gmail.com>
2022-11-11 10:07:26 +0000

    Adding code for One show

--- routes/showR.js
+++ routes/showR.js
@@ -6,8 +6,16 @@ const { User, Show } = require("../models");
 //Validation functions
 
 //GET all Shows
+showRouter.get("/", async (req, res) => {
+    const allShows = await Show.findAll();
+    res.status(200).send(allShows);
+})
 
 //GET one Shows
+showRouter.get("/shows/:num", async (req, res) => {
+    const oneShow = await Show.findByPk(req.params.num);
+    res.status(200).send(oneShow);
+})
 
 //GET Shows in a particular genre [req.params]
 
