d861e7f7050950e3d7a25496231f03473e217f53
Ihs4nn <dfihsan@gmail.com>
2022-11-11 11:43:20 +0000

    Fixing the validations for user/show routes

--- routes/showR.js
+++ routes/showR.js
@@ -2,8 +2,16 @@
 const { Router } = require("express");
 const showRouter = Router();
 const { User, Show } = require("../models");
+const { body, validationResult } = require('express-validator')
 
-//Validation functions
+// //Validation functions
+// async function ratingValid (req, res, next){
+//     req.showRate = await Show.findByPk(req.params.num);
+//     if(req.showRate.rating === "null"){
+//         res.sendStatus(404).send("Error");
+//     }
+//     next()
+// }
 
 //GET all Shows
 showRouter.get("/", async (req, res) => {
@@ -12,16 +20,41 @@ showRouter.get("/", async (req, res) => {
 })
 
 //GET one Shows
-showRouter.get("/shows/:num", async (req, res) => {
+showRouter.get("/:num", async (req, res) => {
     const oneShow = await Show.findByPk(req.params.num);
     res.status(200).send(oneShow);
 })
 
 //GET Shows in a particular genre [req.params]
+showRouter.get("/genres/:category", async (req, res) => {
+    const allCategory = await Show.findAll({
+        where: {genre: req.params.category}
+    })
+    res.status(200).send(allCategory);
+})
 
 //PUT updating rating of a Show that has been watched
+showRouter.put("/:num/watched",
+    
+    body('rating').not().isEmpty()
+    .withMessage("Cant leave it blank!").custom(value => !/\s/.test(value))
+    .withMessage("Cant have white spaces!"), 
+    
+    async (req, res) => {
+
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            res.status(401).send(error);
+        }        
+        const showRate = await Show.findByPk(req.params.num);
+        await showRate.update({
+            rating: req.body.rating
+        })
+        res.status(200).send("Successful");
+})
 
 //PUT update the status of a Show
+
 
 //DELETE a Show
 
--- routes/userR.js
+++ routes/userR.js
@@ -2,15 +2,17 @@
 const { Router } = require("express");
 const userRouter = Router();
 const { User, Show } = require("../models")
+const { body, validationResult } = require('express-validator')
+
 //Validation function for the PUT method
 
-async function userValidation (req, res, next) {
-    req.show = await Show.findByPk(req.params.showNum)
-    if(req.show.status === "null" || req.show.status.includes(" ") || req.show.status.length > 25 || req.show.status.length < 5){
-        res.sendStatus(404)
-    }
-    next()
-}
+// async function userValidation (req, res, next) {
+//     req.show = await Show.findByPk(req.params.showNum)
+//     if(req.show.status === "null" || req.show.status.includes(" ") || req.show.status.length > 25 || req.show.status.length < 5){
+//         res.sendStatus(404)
+//     }
+//     next()
+// }
 
 //GET all User
 userRouter.get("/", async (req, res) => {
@@ -32,12 +34,25 @@ userRouter.get("/:num/shows", async (req, res) => {
 })
 
 //PUT update and add a show if a User has watched it
-userRouter.put("/:userNum/shows/:showNum", userValidation, async (req, res) => {
-    const user = await User.findByPk(req.params.userNum)
-    // const show = await Show.findByPk(req.params.showNum)
+userRouter.put("/:userNum/shows/:showNum", 
+    //Actual validation of the status w/messages
+    body('status').not().isEmpty()
+    .withMessage("Cant leave it blank!").custom(value => !/\s/.test(value))
+    .withMessage("Cant have white spaces!").isLength({max: 25}).isLength({min: 5})
+    .withMessage("Must be between 5-25 characters"),
+    
+    //Updates the user after the validation
+    async (req, res) => {
+        //Sends error if there is an error obviously
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            res.status(401).send(error)
+        }
+        const user = await User.findByPk(req.params.userNum)
+        const show = await Show.findByPk(req.params.showNum)
     //when a user has watched a show it should add that userId to the show
-    await req.show.setUser(user);
-    res.status(200).send(req.show);
+        await show.setUser(user);
+        res.status(200).send("Successful");
 })
 
 module.exports = userRouter;
